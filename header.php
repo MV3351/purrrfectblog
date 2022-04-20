4024608b4f9bf627ed7c32db7e8c2eff38e21ff8
Madison Goss <madisongoss4211@gmail.com>
2022-04-20 14:31:20 -0700

    added bs5 and nav

---
+++ header.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
+    <?php wp_head(); ?>    
+  </head>
+  <body>
+    
+
+
+<!-- NAVBAR -->
+<nav class="navbar navbar-expand-lg navbar-light custom-nav border-bottom fixed-top">
+  <div class="container mx-5">
+      <div class="logo">
+            <a class="navbar-brand" href="<?php echo bloginfo( 'url' ) ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>./img/wordlogo.png" alt="">
+            </a>
+        </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php
+            wp_nav_menu(array(
+                'theme_location' => 'main-menu',
+                'container' => false,
+                'menu_class' => '',
+                'fallback_cb' => '__return_false',
+                'items_wrap' => '<ul id="%1$s" class="navbar-nav me-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
+                'depth' => 2,
+                'walker' => new bootstrap_5_wp_nav_menu_walker()
+            ));
+        ?>
+      <form class="d-flex" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+      <div class="input-group">
+        <input type="text" class="form-control form-control-sm" placeholder="<?php echo esc_attr_x( 'Search for …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>">
+        <button class="btn btn-sm btn-success" type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+        </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" >
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
-    <?php wp_head(); ?>    
-  </head>
-  <body>
+<?php get_header(); ?>
     
 
 
-    <?php wp_footer(); ?>
-  </body>
-</html>
+<?php get_footer(); ?>
