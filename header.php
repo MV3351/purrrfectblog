8b67c0be8aad798f98a9ff4a5035f85fde7db9cf
Madison Goss <madisongoss4211@gmail.com>
2022-04-25 10:55:00 -0700

    trying to fix logo img

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
 <nav class="navbar navbar-expand-lg navbar-light custom-nav border-bottom fixed-top">
   <div class="container mx-5">
       <div class="logo">
-            <a class="navbar-brand" href="<?php echo bloginfo( 'url' ) ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>./img/wordlogo.png" alt="">
+            <a class="navbar-brand" href="<?php echo bloginfo( 'url' ); ?>">
+                <img src="<?php echo get_template_directory_uri()  . '/img/wordlogo.png' ;?>" alt="">
             </a>
         </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,7 +37,7 @@
         ?>
       <form class="d-flex" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
       <div class="input-group">
-        <input type="text" class="form-control form-control-sm" placeholder="<?php echo esc_attr_x( 'Search for …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>">
+        <input type="text" class="form-control form-control-sm" placeholder="<?php echo esc_attr_x( 'Search for …', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ); ?>">
         <button class="btn btn-sm btn-success" type="submit">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
